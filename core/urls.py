8280fd62034610c81e4b4ad8feae684c3e940916
Hordunlarmy <Hordunlarmy@gmail.com>
2025-01-24 09:38:58 +0100

    feat: Add Swagger UI for API documentation

--- core/urls.py
+++ core/urls.py
@@ -27,12 +27,12 @@ from .views import api_ping, ping
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="My API",
+        title="Task Manager API",
         default_version="v1",
-        description="My API description",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="Awesome License"),
+        description="API for managing tasks",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="horduntech@gmail.com"),
+        license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -41,6 +41,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("", ping, name="health_check"),
     path("api/", api_ping, name="api-health_check"),
+    path(
+        "swagger<format>/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
